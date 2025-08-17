c397f40d458b37bdbc03212def0fb1d693e42527
Martin Seibert <martin.seibert@seibert.group>
2025-08-17 13:03:44 +0200

    Delete .vercel directory

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, a as start } from './chunks/_@astrojs-ssr-adapter_CfVy2S7P.mjs';
--- .vercel/output/_functions/chunks/consts_Dt6daOuA.mjs
+++
@@ -1,4 +0,0 @@
-const SITE_TITLE = "Orange Singers";
-const SITE_DESCRIPTION = "Offizielle Website der Orange Singers der Oranienschule Wiesbaden";
-
-export { SITE_DESCRIPTION as S, SITE_TITLE as a };
